9f3f704a12a1d5a6c6b950d0e15bb744835e8fc1
PavelKarmazin <karmazind1@gmail.com>
2021-10-17 16:40:31 +0300

    Функция изменения файла

--- Function.py
+++ Function.py
@@ -1,13 +1,19 @@
-import os
-def cr_file():
+def cr_file(text):
     file = open('Holms.txt', 'w')
-    file.write('Hello, Mr. Holmes')
+    file.write(text)
     file.close()
 
 def ch_file():
+    L = []
+    file = open('Holms.txt', 'a+')
+    for line in open('Holms.txt', 'r'):
+        L.append(line)
+    a = L[0]
+    newString = a[ :11] + "Sherlock " + a[11:] + "!"
     file = open('Holms.txt', 'w')
-    file.write('Hello, Mr. Sherlock Holmes!')
+    file.write(newString)
     file.close()
+            
 
 def add_to_file(path, s):
     try:
@@ -17,6 +23,6 @@ def add_to_file(path, s):
     except FileNotFoundError:
         print('Невозможно открыть файл')
 
-#cr_file()
-#ch_file()
-add_to_file('G:/PyProjects/FileWork/Holms.txt', 'Hello')
+cr_file('Hello, mr. Holmes')
+ch_file()
+#add_to_file('G:/PyProjects/FileWork/Holms.txt', 'Hello')
